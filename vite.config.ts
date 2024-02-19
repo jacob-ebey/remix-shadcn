b2cca3846b904525d9f239feba294c8fbfae1e8f
Jacob Ebey <jacob.ebey@live.com>
2024-02-18 18:02:10 -0800

    chore: add vite-env-only chore: enable v3 future flags

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { unstable_vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
+import envOnly from "vite-env-only";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-	plugins: [remix(), tsconfigPaths()],
+	plugins: [
+		envOnly(),
+		tsconfigPaths(),
+		remix({
+			future: {
+				v3_fetcherPersist: true,
+				v3_relativeSplatPath: true,
+				v3_throwAbortReason: true,
+			},
+		}),
+	],
 });
