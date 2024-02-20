345e5b18a3a8b8d5d6387818fc1ae8eda07ad54b
Jacob Ebey <jacob.ebey@live.com>
2024-02-20 13:27:08 -0800

    feat: add global pending indicator

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,5 @@
 function lightDarkVar(baseName) {
-	return `var(--theme-light, hsl(var(--${baseName}))) var(--theme-dark, hsl(var(--${baseName}-dark)))`;
+  return `var(--theme-light, hsl(var(--${baseName}))) var(--theme-dark, hsl(var(--${baseName}-dark)))`;
 }
 
 /** @type {import('tailwindcss').Config} */
